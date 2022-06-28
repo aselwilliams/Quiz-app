36c0527915fee07054b882f9f56a70d8e886d502
aselwilliams <aselwilliams22@gmail.com>
2022-06-28 11:29:33 -0400

    q and a displayed

---
+++ src/QBank.js
@@ -0,0 +1,26 @@
+export const Questions=[
+    {
+        prompt:'Which dinosaur could swim?',
+        optionA:'Diplodocus',
+        optionB:'Triceratops',
+        optionC:'T-rex',
+        optionD:'Spinosaurus',
+        answer:'D'
+    },
+    {
+        prompt:'Which one means flying reptile?',
+        optionA:'Elasmosaurus',
+        optionB:'Maiasaura',
+        optionC:'Pterodactyl',
+        optionD:'Spinosaurus',
+        answer:'C'
+    },
+    {
+        prompt:'Which dinosaur had body armour?',
+        optionA:'Gigantosaurus',
+        optionB:'Maiasaura',
+        optionC:'Ankylosaurus',
+        optionD:'Spinosaurus',
+        answer:'C'
+    }
+]
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React,{useState} from 'react';
+import {Questions} from '../QBank'
 
 function Quiz() {
+    const [currQuestion, setCurrQuestion]=useState(0)
+    const [chosen, setChosen]=useState('')
+
+    const nextQuestion=()=>{
+        
+    }
   return (
-    <div>Quiz</div>
+    <div className='quiz'>
+        <h1>{Questions[currQuestion].prompt}</h1>
+        <div className='options'>
+            <button onClick={()=>setChosen('A')}>{Questions[currQuestion].optionA}</button>
+            <button onClick={()=>setChosen('B')}>{Questions[currQuestion].optionB}</button>
+            <button onClick={()=>setChosen('C')}>{Questions[currQuestion].optionC}</button>
+            <button onClick={()=>setChosen('D')}>{Questions[currQuestion].optionD}</button>
+        </div>
+        <button onClick={()=>setCurrQuestion(currQuestion<Questions.length-1 ? currQuestion+1 : 0)}>Next Question</button>
+    </div>
   )
 }
 
