87ea6deba623f24674c0e280d72c2c08f4cae161
aselwilliams <aselwilliams22@gmail.com>
2022-06-28 10:21:37 -0400

    components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-
+import {useState} from 'react'
 import './App.css';
+import EndScreen from './components/EndScreen';
+import MainMenu from './components/MainMenu';
+import Quiz from './components/Quiz'
 
 function App() {
+  const [gameState, setGameState]=useState('menu')
   return (
     <div className="App">
-   
+   <h1>Quiz App</h1>
+   {gameState==='menu' && <MainMenu />}
+   {gameState==='quiz' && <Quiz />}
+   {gameState==='endScreen' && <EndScreen />}
     </div>
   );
 }
---
+++ src/components/MainMenu.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MainMenu() {
+  return (
+    <div className='menu'>
+        <button>Start Quiz</button>
+    </div>
+  )
+}
+
+export default MainMenu
